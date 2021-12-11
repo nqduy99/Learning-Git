940660f7445ca668a2153bdd56d47951676c4007
Nguyễn Quang Duy <Quang Duy>
2021-12-11 20:36:11 +0700

    Exports Dog.js and edit index.js

--- Dog.js
+++ Dog.js
@@ -1,5 +1,8 @@
 function Dog(){
+    this.name = "Luca"
     Dog.prototype.eat = function(cat){
         this.stomach.push(cat);
     }
 }
+
+module.exports = Dog;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var Dog = require('./Dog')
+
 function Cat(){
 
 }
